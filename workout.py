fe761b879bfe9615f0874832b795536d3305e376
michaelzcjia <michael.zc.jia@gmail.com>
2018-12-03 22:43:43 -0500

    small changes to get workout working

--- workout.py
+++ workout.py
@@ -104,6 +104,7 @@ class Workout:
             #Add exercise name
             self.workout[exs[i]]['ex'] = ex.Name.iloc[0]
             #Add exercise duration
+            print(ex_burn,ex.MET.iloc[0],user.weight)
             self.workout[exs[i]]['dur'] = int(60*(ex_burn / (ex.MET.iloc[0] * float(user.weight) * 0.453592 * 4)))
             #Add exercise description
             self.workout[exs[i]]['descr'] = ex.Description.iloc[0]
@@ -118,8 +119,8 @@ class Workout:
     def calc_cal(self, pref, user):
         "Returns the daily caloric burn"
         #Note: make sure to do type conversion (string -> float)
-        w = int(user.weight)
-        h = int(user.height)
+        w = float(user.weight)
+        h = float(user.height)
         a = int(user.age)
         s = user.sex
         m_cal = self.calc_maint(w,h,a,s)
